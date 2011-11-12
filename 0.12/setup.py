70628e9c3fb199a74547b6af93e73c68d9d1f473
lazaridis-com <ilias@lazaridis.com>
2011-11-12 09:52:01 +0200

    changes for 0.12 compatibility, as described in http://vanalboom.org/node/15

--- 0.12/setup.py
+++ 0.12/setup.py
@@ -8,13 +8,13 @@ VERSION = '0.6.0'
 setup (
     name = PACKAGE,
     version = VERSION,
-    description = 'LDAP extensions for Trac 0.11',
+    description = 'LDAP extensions for Trac 0.12',
     author = 'Emmanuel Blot',
     author_email = 'emmanuel.blot@free.fr',
     license='BSD', 
     url='http://trac-hacks.org/wiki/LdapPlugin',
     keywords = "trac ldap permission group acl",
-    install_requires = [ 'Trac>=0.11', 'Trac<0.12'],
+    install_requires = [ 'Trac>=0.12', 'Trac<0.13'],
     packages = find_packages(exclude=['ez_setup', '*.tests*']),
     package_data = { },
     entry_points = {
